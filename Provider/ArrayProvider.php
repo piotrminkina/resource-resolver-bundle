2f8f7472ccfd15018ac9cc52c79cf1d3df63611b
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-04-16 22:40:03 +0200

    Extracted ArrayProvider from RequestProvider

---
+++ Provider/ArrayProvider.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace PMD\ResourcesResolverBundle\Provider;
+
+use PMD\ResourcesResolverBundle\Exception\InvalidArgumentException;
+use PMD\ResourcesResolverBundle\Exception\NotFoundException;
+
+/**
+ * Class ArrayProvider
+ * @package PMD\ResourcesResolverBundle\Provider
+ */
+class ArrayProvider implements ProviderInterface
+{
+    /**
+     * @var array
+     */
+    protected $resources;
+
+    /**
+     * @param array $resources
+     */
+    public function __construct(array $resources = array())
+    {
+        $this->resources = $resources;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function has($name)
+    {
+        return array_key_exists($name, $this->resources);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get($name)
+    {
+        if (!$this->has($name)) {
+            throw new NotFoundException(
+                sprintf(
+                    'Resource with name %name% not exists',
+                    $name
+                )
+            );
+        }
+
+        return $this->resources[$name];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function set($name, $resource)
+    {
+        $this->resources[$name] = $resource;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function add($name, $resource)
+    {
+        if ($this->has($name)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Resource %name% cannot be added because another one exists',
+                    $name
+                )
+            );
+        }
+
+        return $this->set($name, $resource);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function replace($name, $resource)
+    {
+        if (!$this->has($name)) {
+            throw new NotFoundException(
+                sprintf(
+                    'Resource %name% cannot be replaced because not exists',
+                    $name
+                )
+            );
+        }
+
+        return $this->set($name, $resource);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function remove($name)
+    {
+        unset($this->resources[$name]);
+
+        return $this;
+    }
+}
--- Provider/RequestProvider.php
+++ Provider/RequestProvider.php
@@ -3,20 +3,13 @@
 namespace PMD\ResourcesResolverBundle\Provider;
 
 use Symfony\Component\HttpFoundation\Request;
-use PMD\ResourcesResolverBundle\Exception\InvalidArgumentException;
-use PMD\ResourcesResolverBundle\Exception\NotFoundException;
 
 /**
  * Class RequestProvider
  * @package PMD\ResourcesResolverBundle\Provider
  */
-class RequestProvider implements ProviderInterface
+class RequestProvider extends ArrayProvider
 {
-    /**
-     * @var array
-     */
-    protected $resources;
-
     /**
      * @param Request $request
      * @param string $storageKey
@@ -24,88 +17,11 @@ class RequestProvider implements ProviderInterface
     public function __construct(Request $request, $storageKey = '_resources')
     {
         if ($request->attributes->has($storageKey)) {
-            $this->resources = $request->attributes->get($storageKey);
+            $resources = $request->attributes->get($storageKey);
         } else {
-            $this->resources = array();
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function has($name)
-    {
-        return array_key_exists($name, $this->resources);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function get($name)
-    {
-        if (!$this->has($name)) {
-            throw new NotFoundException(
-                sprintf(
-                    'Resource with name %name% not exists',
-                    $name
-                )
-            );
+            $resources = array();
         }
 
-        return $this->resources[$name];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function set($name, $resource)
-    {
-        $this->resources[$name] = $resource;
-
-        return $this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function add($name, $resource)
-    {
-        if ($this->has($name)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Resource %name% cannot be added because another one exists',
-                    $name
-                )
-            );
-        }
-
-        return $this->set($name, $resource);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function replace($name, $resource)
-    {
-        if (!$this->has($name)) {
-            throw new NotFoundException(
-                sprintf(
-                    'Resource %name% cannot be replaced because not exists',
-                    $name
-                )
-            );
-        }
-
-        return $this->set($name, $resource);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function remove($name)
-    {
-        unset($this->resources[$name]);
-
-        return $this;
+        parent::__construct($resources);
     }
 }
